a51587764e3f4a3b6b309352ded42564546d20e4
theguffs <youssefabdelmalak01@gmail.com>
2026-01-31 13:44:00 +0100

    esercizi: HelloWorld + tabelline

---
+++ tabelline/MultiplicationTableRunner.java
@@ -0,0 +1,16 @@
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		table.print();
+		// se non inserisco niente mi stampa tabellina del 5.
+		// se metto un altro parametro mi crea la tabellina del parametro scelto.
+		// es. table.print(9);
+		// se metto 3 parametri mi crea la tabellina del primo parametro scelto,
+		// iniziando dal secondo parametro scelto e finendo con il terzo parametro
+		// es. table.print(9,20,30);
+		// scelto.
+	}
+
+}
